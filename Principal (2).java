cfad069585c3f03f3d5008348f4d3ef05ac04b15
luti <luti_souza142@hotmail.com>
2014-03-17 21:15:11 -0300

    Hello You
    Hello You

---
+++ Principal (2).java
@@ -0,0 +1,12 @@
+
+public class Principal 
+{
+	public static void main(String[] args) 
+	{
+		 System.out.println("Hello World");
+		 String Hello;
+		 String nome = "Laila";
+		 System.out.println ("Hello " + nome);
+	}
+
+}
